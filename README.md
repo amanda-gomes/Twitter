5fbbac18dcf48a10a4a9be9ef063680c29fbecbb
amanda-gomes <amacrisgomes@hotmail.com>
2019-02-20 16:38:12 -0300

    versão 0.0.6 finalizada

---
+++ README.md
@@ -0,0 +1,16 @@
+# Freelancer
+
+## Objetivo
+
+Replicar o site **Freelancer**, com o  seguinte resultado:
+
+![Freelancer Website](docs/fullpage.png)
+
+## Considerações
+
+* No arquivo base `index.html` escrevi a estrutura do projeto (utilizei o HTML semântico).
+
+* Na pasta `css` há o arquivo-base `style.css` com os estilos necessários para estilizar a página html.
+
+* Dentro da pasta `assets` se encontra a pasta `images` onde estão todas as imagens.
+
---
+++ js/twitter.js
@@ -0,0 +1,49 @@
+document.getElementById("twittar").addEventListener("click", twittar);
+document.getElementById("tweet").addEventListener("input", contarCaracteres);
+document.getElementById("tweet").addEventListener("keyup", tamTextarea);
+
+function twittar() {
+  let tweet = document.getElementsByTagName("textarea")[0];
+  let tweets = document.getElementsByTagName("ul")[0];
+  let newTweet = document.createElement("li");
+  let contador = document.getElementsByTagName("strong")[0];
+  let btnTwittar = document.getElementById("twittar");
+  let horas = new Date().getHours() + ":" + new Date().getMinutes();
+  newTweet.innerHTML = horas + " - <b>Você</b>:<br>" + tweet.value;
+  tweets.appendChild(newTweet);
+  tweet.value = "";
+  tweet.rows = 1;
+  contador.textContent = 140;
+  contador.setAttribute("class", "black")
+  btnTwittar.setAttribute("disabled", "");
+}
+
+function contarCaracteres() {
+  let tweet = document.getElementsByTagName("textarea")[0];
+  let contagem = 140 - tweet.value.length;
+  let contador = document.getElementsByTagName("strong")[0];
+  contador.textContent = contagem;
+  let btnTwittar = document.getElementById("twittar");
+
+  if (contagem === 140 || contagem < 0) {
+    btnTwittar.setAttribute("disabled", "");
+  } else {
+    btnTwittar.removeAttribute("disabled", "");
+  }
+
+  if (contagem < 20 && contagem >= 10) {
+    contador.setAttribute("class", "claro");
+  } else if (contagem < 10) {
+    contador.setAttribute("class", "escuro");
+  } else {
+    contador.setAttribute("class", "black");
+  }
+}
+
+function tamTextarea() {
+  let tweet = document.getElementById('tweet');
+
+  if (tweet.scrollHeight > tweet.offsetHeight) {
+    tweet.rows += 1;
+  }
+}
